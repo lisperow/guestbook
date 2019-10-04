6aa40924e940622ad2cb960f377e7952c64c288f
lisperow <lisperow@yandex.ru>
2019-10-04 23:10:41 +0300

    add guestbook table

--- src/server/guestbook/db.cljs
+++ src/server/guestbook/db.cljs
@@ -4,3 +4,20 @@
    [mount.core :refer [defstate]]))
 
 (def sqlite3 (node/require "sqlite3"))
+
+(defstate db
+  :start (let [db (sqlite3.Database. ":memory")]
+           (.run
+            db
+            "CREATE TABLE guestbook
+              (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  name VARCHAR(30),
+                  message VARCHAR(30),
+                  time TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"))
+  :stop (.close @db))
+
+(defn add-message [{:keys [name message]}]
+  (.run @db "INSERT INTO guestbook (name, message) VALUES (?, ?)" #js [name message]))
+
+(defn messages [handler]
+  (.all @db "SELECT * FROM guestbook" handler))
